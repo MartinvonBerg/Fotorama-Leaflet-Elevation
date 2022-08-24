cbcb4ef8ea89c1b9b57ef9462353ad6fe4a56c09
Martin <mail@mvb1.de>
2022-08-24 06:35:31 +0200

    block.json string to number type as quick fix

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '392136f4d6b78929c073204e152eba20');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1a9de91805746d44f7dfd8e627e8c2e8');
