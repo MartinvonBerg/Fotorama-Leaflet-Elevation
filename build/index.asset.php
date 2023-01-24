68b1303aba609857828ef839f61db35fe454aec4
MartinvonBerg <mail@mvb1.de>
2023-01-24 17:58:47 +0100

    webpack testing

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'cebeedf4a47abe5100db');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b3c4180ffff55173e0f9');
